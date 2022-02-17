c93606abd73782faf1195541422794a8978c8fd0
hespoz <hector.espoz@doctari.de>
2022-02-17 17:14:43 +0100

    deploy npm package

--- src/web.ts
+++ src/web.ts
@@ -2,6 +2,40 @@ import { WebPlugin } from '@capacitor/core';
 
 import type { HeapCapacitorBridgePlugin } from './definitions';
 
-// export class HeapCapacitorBridgeWeb extends WebPlugin implements HeapCapacitorBridgePlugin {
+export class HeapCapacitorBridgeWeb extends WebPlugin implements HeapCapacitorBridgePlugin {
+    addEventProperties(options: { properties: object }): Promise<void> {
+        console.log(options);
+        return Promise.resolve(undefined);
+    }
+
+    addUserProperties(options: { properties: object }): Promise<void> {
+        console.log(options);
+        return Promise.resolve(undefined);
+    }
+
+    clearEventProperties(): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+
+    identify(options: { identity: string }): Promise<void> {
+        console.log(options);
+        return Promise.resolve(undefined);
+    }
+
+    removeEventProperty(options: { property: string }): Promise<void> {
+        console.log(options);
+        return Promise.resolve(undefined);
+    }
+
+    resetIdentity(): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+
+    track(options: { event: string }): Promise<void>;
+    track(options: { event: string; properties: object }): Promise<void>;
+    track(options: { event: string } | { event: string; properties: object }): Promise<void> {
+        console.log(options);
+        return Promise.resolve(undefined);
+    }
   
-// }
+}
